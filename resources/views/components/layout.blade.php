6b0b97d547857f83b0d24b110c7af8f13123b132
KrisalisKat <krisaliskat@gmail.com>
2024-10-20 12:31:24 -0500

    Import AlpineJS

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,6 @@
 <head>
     <title>{{ $title ?? '' }}{{ $title && !$skipSiteName ? ' | ' : '' }}{{ !$skipSiteName ? 'Krisalis.@' : '' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body>
 {{ $slot }}
